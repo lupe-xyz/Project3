8b5c79f1bc03fea38351d0f2eef1e8c2f06ea1c7
Lupe Herrera <vgh1996@gmail.com>
2023-11-09 16:11:26 -0800

    2nd commit test

--- src/csc130_project_3/CSC130_Project_3.java
+++ src/csc130_project_3/CSC130_Project_3.java
@@ -15,6 +15,7 @@ public class CSC130_Project_3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
