f5a9da59a78fd26a30f2579fc38ebecb528c22bd
Lupe Herrera <vgh1996@gmail.com>
2023-11-09 16:02:35 -0800

    1st commit

---
+++ src/csc130_project_3/CSC130_Project_3.java
@@ -0,0 +1,20 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package csc130_project_3;
+
+/**
+ *
+ * @author lupe.xyz
+ */
+public class CSC130_Project_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
