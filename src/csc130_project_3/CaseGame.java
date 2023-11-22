73bf4764dd700d5002e444ba25df024eb1f9778e
Lupe Herrera <vgh1996@gmail.com>
2023-11-22 00:18:36 -0800

    Created defaultCaseArray to hold values of the cases

--- src/csc130_project_3/CaseGame.java
+++ src/csc130_project_3/CaseGame.java
@@ -1,25 +1,47 @@
- /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package csc130_project_3;
 
-/**
- *
- * @author lupe.xyz
- */
-public class CaseGame {
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
+
+public class CaseGame {
     
-    public static void testMethod() {
-        //
-        //adding stuff
-        //adding more stuff
+    
+    public static void printArray(Integer[] array) {
+        int n = array.length;
+        for(int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.println("Case Game test.");
+        
+        
+        Integer[] defaultCaseArray = { 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 
+                            500, 750, 1000, 5000, 10000, 25000, 50000, 
+                            75000, 100000, 200000, 300000, 400000, 
+                            500000, 750000, 1000000 };
+        
+        
+        printArray(defaultCaseArray);
+        System.out.println();
+        
+        //Shuffling the cases
+        List<Integer> intList;
+        intList = Arrays.asList(defaultCaseArray);
+        Collections.shuffle(intList);
+        intList.toArray(defaultCaseArray);
+        
+        //Printing test
+        printArray(defaultCaseArray);
+        System.out.println("\n" + defaultCaseArray[2]);
+        
+        
+        
         
     
     }
